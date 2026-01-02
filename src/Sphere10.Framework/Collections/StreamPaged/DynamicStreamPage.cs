5b4736959332f524ce9a7c5508d911f975a6b717
Herman Schoenfeld <herman@sphere10.com>
2026-01-03 04:00:08 +1000

    Avoid Guard.Ensure in DynamicStreamPage hot loop

--- src/Sphere10.Framework/Collections/StreamPaged/DynamicStreamPage.cs
+++ src/Sphere10.Framework/Collections/StreamPaged/DynamicStreamPage.cs
@@ -245,8 +245,9 @@ internal class DynamicStreamPage<TItem> : StreamPageBase<TItem> {
 		Array.Copy(sizes, 0, _itemSizes, index, sizes.Length);
 		Stream.Seek(StartPosition + Object0SizeFieldOffset + index * ObjectSizeFieldSize, SeekOrigin.Begin);
 		foreach (var size in sizes) {
-			Guard.Ensure(size >= 0, "Sizes cannot be negative");
-			Writer.Write(checked((ulong)size));
+			if (size < 0)
+				throw new InvalidDataException("Sizes cannot be negative");
+			Writer.Write(unchecked((ulong)size));
 		}
 
 		// Calculate the updated offsets if opened
