dfc8f2a86e38ee09b31c62b2cfc2272be18ba7b8
Herman Schoenfeld <herman@sphere10.com>
2026-03-30 00:51:33 +1000

    Register all new screens in SystemExpertBlock and add required NuGet packages

--- utils/SystemExpert/SystemExpertBlock.cs
+++ utils/SystemExpert/SystemExpertBlock.cs
@@ -8,11 +8,17 @@ public class SystemExpertBlock : ApplicationBlock {
 	public static ApplicationBlock Build() {
 		return new ApplicationBlockBuilder()
 			.WithName("System Expert")
-			.WithDefaultScreen<ProcessesScreen>()
+			.WithDefaultScreen<SystemInfoScreen>()
 			.AddMenu(mb => mb
 				.WithText("Tools")
+				.AddScreenItem<SystemInfoScreen>("System Info", null)
 				.AddScreenItem<ProcessesScreen>("Processes", null)
+				.AddScreenItem<ServicesScreen>("Services", null)
+				.AddScreenItem<NetworkScreen>("Network", null)
+				.AddScreenItem<EventLogScreen>("Event Log", null)
+				.AddScreenItem<EnvironmentScreen>("Environment", null)
 			)
 			.Build();
 	}
 }
+
