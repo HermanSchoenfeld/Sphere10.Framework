060bb5109fcbf639c285b8112fde3b9b08559c93
Herman Schoenfeld <herman@sphere10.com>
2026-03-31 04:42:43 +1000

    Add ModuleConfiguration for MSSQL, SQLite, and Firebird WinForms providers

---
+++ src/Sphere10.Framework.Windows.Forms.Sqlite/ModuleConfiguration.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Herman Schoenfeld 2018 - Present. All rights reserved. (https://sphere10.com)
+// Author: Herman Schoenfeld
+//
+// Distributed under the MIT NON-AI software license, see the accompanying file
+// LICENSE or visit https://sphere10.com/legal/NON-AI-MIT.
+//
+// This notice must not be removed when duplicating this file or its contents, in whole or in part.
+
+using Microsoft.Extensions.DependencyInjection;
+using Sphere10.Framework.Application;
+using Sphere10.Framework.Data;
+using Sphere10.Framework.Windows.Forms;
+
+namespace Sphere10.Framework.Windows.Forms.Sqlite;
+
+public class ModuleConfiguration : ModuleConfigurationBase {
+	public override void RegisterComponents(IServiceCollection serviceCollection) {
+		serviceCollection.AddNamedTransient<ConnectionBarBase, SqliteConnectionBar>(nameof(DBMSType.Sqlite));
+		serviceCollection.AddNamedTransient<ConnectionPanelBase, SqliteConnectionPanel>(nameof(DBMSType.Sqlite));
+	}
+}
