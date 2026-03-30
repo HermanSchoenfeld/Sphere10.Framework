55035d932cbcd34e125013302c05dc9aff678287
Herman Schoenfeld <herman@sphere10.com>
2026-03-31 04:43:17 +1000

    Add Sphere10.Framework.Windows.Forms.MySQL project with ConnectionBar and ConnectionPanel controls

---
+++ src/Sphere10.Framework.Windows.Forms.MySQL/MySQLConnectionBar.cs
@@ -0,0 +1,74 @@
+// Copyright (c) Herman Schoenfeld 2018 - Present. All rights reserved. (https://sphere10.com)
+// Author: Herman Schoenfeld
+//
+// Distributed under the MIT NON-AI software license, see the accompanying file
+// LICENSE or visit https://sphere10.com/legal/NON-AI-MIT.
+//
+// This notice must not be removed when duplicating this file or its contents, in whole or in part.
+
+using System.ComponentModel;
+using MySqlConnector;
+using Sphere10.Framework.Data;
+
+namespace Sphere10.Framework.Windows.Forms.MySQL;
+
+public partial class MySQLConnectionBar : ConnectionBarBase {
+	public MySQLConnectionBar() {
+		InitializeComponent();
+	}
+
+	protected override IDAC GetDACInternal() {
+		return new MySQLDAC(ConnectionString);
+	}
+
+	[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+	[Browsable(false)]
+	public override string ConnectionString {
+		get {
+			var builder = new MySqlConnectionStringBuilder();
+			builder.Server = _serverTextBox.Text;
+			builder.Database = _databaseTextBox.Text;
+			builder.UserID = _usernameTextBox.Text;
+			builder.Password = _passwordTextBox.Text;
+			var port = Tools.Parser.Parse<int?>(_portTextBox.Text);
+			if (port.HasValue)
+				builder.Port = (uint)port.Value;
+			return builder.ToString();
+		}
+		set {
+			var builder = new MySqlConnectionStringBuilder(value);
+			_serverTextBox.Text = builder.Server;
+			_databaseTextBox.Text = builder.Database;
+			_usernameTextBox.Text = builder.UserID;
+			_passwordTextBox.Text = builder.Password;
+			_portTextBox.Text = builder.Port != 3306 ? builder.Port.ToString() : string.Empty;
+		}
+	}
+
+	public override string DatabaseName => Database;
+
+	public string Server {
+		get { return _serverTextBox.Text; }
+		set { _serverTextBox.Text = value; }
+	}
+
+	public string Database {
+		get { return _databaseTextBox.Text; }
+		set { _databaseTextBox.Text = value; }
+	}
+
+	public string Username {
+		get { return _usernameTextBox.Text; }
+		set { _usernameTextBox.Text = value; }
+	}
+
+	public string Password {
+		get { return _passwordTextBox.Text; }
+		set { _passwordTextBox.Text = value; }
+	}
+
+	public string Port {
+		get { return _portTextBox.Text; }
+		set { _portTextBox.Text = value; }
+	}
+}
