d6035559417d480c48a838218fa29a99e4b7b07d
Herman Schoenfeld <herman@sphere10.com>
2026-03-31 08:06:07 +1000

    Add SplashScreen form with fade in/out, framework lifecycle title updates, and SystemLog subtitle integration

---
+++ src/Sphere10.Framework.Windows.Forms/Dialogs/SplashScreen.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Sphere10.Framework.Windows.Forms;
+
+partial class SplashScreen {
+	/// <summary>
+	/// Required designer variable.
+	/// </summary>
+	private System.ComponentModel.IContainer components = null;
+
+	/// <summary>
+	/// Clean up any resources being used.
+	/// </summary>
+	/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+	protected override void Dispose(bool disposing) {
+		if (disposing && (components != null)) {
+			components.Dispose();
+		}
+		base.Dispose(disposing);
+	}
+
+	#region Windows Form Designer generated code
+
+	/// <summary>
+	/// Required method for Designer support - do not modify
+	/// the contents of this method with the code editor.
+	/// </summary>
+	private void InitializeComponent() {
+		_pictureBox = new System.Windows.Forms.PictureBox();
+		_titleLabel = new System.Windows.Forms.Label();
+		_subTitleLabel = new System.Windows.Forms.Label();
+		((System.ComponentModel.ISupportInitialize)(_pictureBox)).BeginInit();
+		SuspendLayout();
+		// 
+		// _pictureBox
+		// 
+		_pictureBox.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+		_pictureBox.Location = new System.Drawing.Point(0, 0);
+		_pictureBox.Name = "_pictureBox";
+		_pictureBox.Size = new System.Drawing.Size(600, 340);
+		_pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+		_pictureBox.TabIndex = 0;
+		_pictureBox.TabStop = false;
+		// 
+		// _titleLabel
+		// 
+		_titleLabel.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+		_titleLabel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+		_titleLabel.ForeColor = System.Drawing.Color.FromArgb(64, 64, 64);
+		_titleLabel.Location = new System.Drawing.Point(12, 348);
+		_titleLabel.Name = "_titleLabel";
+		_titleLabel.Size = new System.Drawing.Size(576, 22);
+		_titleLabel.TabIndex = 1;
+		// 
+		// _subTitleLabel
+		// 
+		_subTitleLabel.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+		_subTitleLabel.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+		_subTitleLabel.ForeColor = System.Drawing.Color.Gray;
+		_subTitleLabel.Location = new System.Drawing.Point(12, 372);
+		_subTitleLabel.Name = "_subTitleLabel";
+		_subTitleLabel.Size = new System.Drawing.Size(576, 20);
+		_subTitleLabel.TabIndex = 2;
+		// 
+		// SplashScreen
+		// 
+		AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+		AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+		BackColor = System.Drawing.Color.White;
+		ClientSize = new System.Drawing.Size(600, 400);
+		ControlBox = false;
+		Controls.Add(_titleLabel);
+		Controls.Add(_subTitleLabel);
+		Controls.Add(_pictureBox);
+		FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+		Name = "SplashScreen";
+		ShowInTaskbar = false;
+		StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+		TopMost = true;
+		((System.ComponentModel.ISupportInitialize)(_pictureBox)).EndInit();
+		ResumeLayout(false);
+	}
+
+	#endregion
+
+	private System.Windows.Forms.PictureBox _pictureBox;
+	private System.Windows.Forms.Label _titleLabel;
+	private System.Windows.Forms.Label _subTitleLabel;
+}
---
+++ src/Sphere10.Framework.Windows.Forms/Dialogs/SplashScreen.cs
@@ -0,0 +1,115 @@
+﻿// Copyright (c) Herman Schoenfeld 2018 - Present. All rights reserved. (https://sphere10.com)
+// Author: Herman Schoenfeld
+//
+// Distributed under the MIT NON-AI software license, see the accompanying file
+// LICENSE or visit https://sphere10.com/legal/NON-AI-MIT.
+//
+// This notice must not be removed when duplicating this file or its contents, in whole or in part.
+
+using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using Sphere10.Framework.Application;
+
+namespace Sphere10.Framework.Windows.Forms;
+
+public partial class SplashScreen : Form {
+	private SplashLogger _logger;
+
+	public SplashScreen() {
+		InitializeComponent();
+	}
+
+	public SplashScreen(Image splashImage)
+		: this() {
+		SplashImage = splashImage;
+	}
+
+	public Image SplashImage {
+		get => _pictureBox.Image;
+		set => _pictureBox.Image = value;
+	}
+
+	public void UpdateTitle(string text) {
+		_titleLabel.Text = text;
+		Update();
+		System.Windows.Forms.Application.DoEvents();
+	}
+
+	public void UpdateSubTitle(string text) {
+		_subTitleLabel.Text = text;
+		Update();
+		System.Windows.Forms.Application.DoEvents();
+	}
+
+	public void FadeIn(int durationMs = 500) {
+		Opacity = 0;
+		Show();
+		var Steps = 20;
+		var StepDelay = durationMs / Steps;
+		for (var I = 1; I <= Steps; I++) {
+			Opacity = (double)I / Steps;
+			Update();
+			Thread.Sleep(StepDelay);
+			System.Windows.Forms.Application.DoEvents();
+		}
+	}
+
+	public void FadeOut(int durationMs = 500) {
+		var Steps = 20;
+		var StepDelay = durationMs / Steps;
+		for (var I = 0; I < Steps; I++) {
+			Opacity = 1.0 - (double)(I + 1) / Steps;
+			Update();
+			Thread.Sleep(StepDelay);
+			System.Windows.Forms.Application.DoEvents();
+		}
+		Close();
+	}
+
+	public void AttachToFramework() {
+		var Framework = Sphere10Framework.Instance;
+		Framework.Registering += OnFrameworkRegistering;
+		Framework.Initializing += OnFrameworkInitializing;
+		Framework.Finalizing += OnFrameworkFinalizing;
+		_logger = new SplashLogger(this);
+		SystemLog.RegisterLogger(_logger);
+	}
+
+	public void DetachFromFramework() {
+		var Framework = Sphere10Framework.Instance;
+		Framework.Registering -= OnFrameworkRegistering;
+		Framework.Initializing -= OnFrameworkInitializing;
+		Framework.Finalizing -= OnFrameworkFinalizing;
+		if (_logger != null) {
+			SystemLog.DeregisterLogger(_logger);
+			_logger = null;
+		}
+	}
+
+	protected override void OnFormClosing(FormClosingEventArgs e) {
+		DetachFromFramework();
+		base.OnFormClosing(e);
+	}
+
+	private void OnFrameworkRegistering() => UpdateTitle("Registering Modules...");
+
+	private void OnFrameworkInitializing() => UpdateTitle("Initializing...");
+
+	private void OnFrameworkFinalizing() => UpdateTitle("Shutting Down...");
+
+	private class SplashLogger : LoggerBase {
+		private readonly SplashScreen _splash;
+
+		public SplashLogger(SplashScreen splash) {
+			_splash = splash;
+			Options = LogOptions.InfoEnabled;
+		}
+
+		protected override void Log(LogLevel logLevel, string message) {
+			if (logLevel == LogLevel.Info && _splash.IsHandleCreated && !_splash.IsDisposed)
+				_splash.UpdateSubTitle(message);
+		}
+	}
+}
